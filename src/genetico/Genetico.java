a8836b3bd2039884e2407b68092ac189eb250b3e
Soulglory <lucabadue@gmail.com>
2019-08-24 15:45:43 -0300

    continuando

--- src/genetico/Genetico.java
+++ src/genetico/Genetico.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package genetico;
 
+import java.util.ArrayList;
+import java.util.Random;
 /**
  *
  * @author lucas
  */
 public class Genetico {
+    //configuracao dos parametros do AG
+    public static final double TAXADEMUTACAO = 0.05;
+    static final double TAXADECRUZAMENTO = 0.9;
+    
+    static final boolean ELITISMO = true;
+    static final boolean ESTAGNA = true; //controle de estagnacao
+    static final double VALORESTAGNA = 200;
+    
+    static final int TAMANHODAPOPULACAO = 400;
+    static final int MAXIMODEGERACOES = 1000;
+    
+    //--------------------------
+    
+    private Populacao populacao = new Populacao();
+    private final Random r = new Random();
+    private int contEstagnar;
+    private double melhorAptidaoAnterior = -1;
+    
+    
     
 }
--- src/genetico/Individuo.java
+++ src/genetico/Individuo.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package genetico;
 
+import java.util.Arrays;
+import java.util.Random;
 /**
  *
  * @author lucas
  */
 public class Individuo {
     
+    private final Random r = new Random();
+    private double aptidao;
+    
+    private double qtdDinheiro;
+    private double qtdPeso;
+    
+    public Individuo(){
+        do{
+            this.qtdDinheiro();
+            this.qtdPeso();
+        } while (!validar());
+        avaliar();
+    }
+    
+    public Individuo(double[] genes){
+        qtdDinheiro = genes[0];
+        qtdPeso = genes[1];
+        
+        if(random.nextDouble() <= Genetico.TAXADEMUTACAO){
+            int posAleatoria = random.nextInt(genes.length);
+            mutacao(posAleatoria);
+        }
+        avaliar();
+    }
+    
+    private boolean validar(){
+        double 
+    }
+
+    public void setQtdDinheiro(double qtdDinheiro) {
+        this.qtdDinheiro = r.nextDouble();
+    }
+
+    public void setQtdPeso(double qtdPeso) {
+        this.qtdPeso = r.nextDouble();
+    }
+    
+    public double getAptidao(){
+        return aptidao;
+    }
 }
